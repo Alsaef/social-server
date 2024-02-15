2f2e31eb1afa8baa0e4035d7f866b081da44b1d9
ASR <saef.ratul@gmail.com>
2024-02-16 00:24:01 +0600

    developer

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -3,7 +3,7 @@ const express=require('express');
 const userController=require('../Controller/userController');
 const router=express.Router();
 
-
+router.route('/').post(userController.createUser).get(userController.getUser)
 router.route('/:email').get(userController.getUserOne)
 
 
