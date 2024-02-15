c041de93caef7d35c8705bbf0d778775d94f8f89
ASR <saef.ratul@gmail.com>
2024-02-16 00:32:21 +0600

    developer

--- Route/userSubRoute/userSub.js
+++ Route/userSubRoute/userSub.js
@@ -3,7 +3,7 @@ const express=require('express');
 const userController=require('../../Controller/userController');
 const { verifyJwt } = require('../../Token/verifyJwt');
 const router=express.Router();
-
+router.route('/').get(userController.getUser)
 router.route('/stack').get(userController.countUser)
 router.route('/:id').get(userController.getUserOnebyId).patch(userController.updateProfile)
 
