afa6f8dc895be53a5f6c0897bd392d30bc8276c9
ASR <saef.ratul@gmail.com>
2024-05-03 12:22:48 +0600

    dev

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port =process.env.PORT || 3000
 require('dotenv').config()
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173/',
+  origin: '*',
   allowMethods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
 }));
 // DB Connection
