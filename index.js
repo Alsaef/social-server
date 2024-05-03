67236a1ccb5cfff7879a1a84a83ea6a0a4bb89d4
ASR <saef.ratul@gmail.com>
2024-05-03 12:06:06 +0600

    dev

--- index.js
+++ index.js
@@ -20,14 +20,30 @@ const userSub=require('./Route/userSubRoute/userSub')
 const postRouter=require('./Route/postRoute')
 const commentRoute=require('./Route/commentRoute')
 const likeRoute=require('./Route/likeRoute')
-const jwtRoute=require('./Route/jwtRoute')
+// const jwtRoute=require('./Route/jwtRoute')
 
 app.use('/api/v1/user',userRouter)
 app.use('/api/v1/users',userSub)
 app.use('/api/v1/post',postRouter)
 app.use('/api/v1/comment',commentRoute)
 app.use('/api/v1/like',likeRoute)
-app.post('/api/v1/jwt',jwtRoute)
+
+app.post('/api/v1/jwt',(req, res) => {
+  try {
+    const userInfo = req.body;
+
+    const expiration = '7500h';
+    const token = jwt.sign(userInfo, process.env.SECURE_TOKEN, {
+      expiresIn: expiration
+    });
+  
+  //   console.log(token)
+    res.json({ token });
+  } catch (error) {
+    console.error('Error generating token:', error);
+    res.status(500).json({ message: 'Token generation failed' });
+  }
+})
 
 app.get('/', (req, res) => {
   res.send('Server Running!')
