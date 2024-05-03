9d4fd54ddddc86700994488dc995b1a6296a0c4e
ASR <saef.ratul@gmail.com>
2024-05-03 12:33:50 +0600

    dev

--- index.js
+++ index.js
@@ -31,19 +31,36 @@ app.use('/api/v1/comment',commentRoute)
 app.use('/api/v1/like',likeRoute)
 
 app.post('/api/v1/jwt',(req, res) => {
-  try {
-    const userInfo = req.body;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    const expiration = '7500h';
-    const token = jwt.sign(userInfo, process.env.SECURE_TOKEN, {
-      expiresIn: expiration
-    });
-  
-  //   console.log(token)
-    res.json({ token });
+  if (req.method === 'OPTIONS') {
+    // Preflight request
+    res.status(200).end();
+    return;
+  }
+  try {
+    if (req.method === 'POST') {
+      const userInfo = req.body;
+
+      if (!userInfo) {
+        throw new Error('User information is missing');
+      }
+
+      const expiration = '7500h';
+      const token = jwt.sign(userInfo, process.env.SECURE_TOKEN, {
+        expiresIn: expiration
+      });
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ token }));
+    } else {
+      throw new Error('Unsupported method');
+    }
   } catch (error) {
     console.error('Error generating token:', error);
-    res.status(500).json({ message: 'Token generation failed' });
+    res.status(500).json({ message: 'Token generation failed', error: error.message });
   }
 })
 
